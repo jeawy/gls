4935024c9f48e16e15169923080c1f4a7a88f501
jeawyzhang <281475120@163.com>
2017-12-09 15:08:26 +0800

    手机

--- 线框图jpg/论坛_07_04/论坛_0705/js/base.js
+++ 线框图jpg/论坛_07_04/论坛_0705/js/base.js
@@ -8,3 +8,16 @@ location.href = 'https://weibo.com/asu001';
 jQuery(document).ready(function(){
 jQuery('ul.spinner').width(jQuery('button.spinner_btn').innerWidth());
 });
+
+/* 输入字数监听 */
+jQuery("#posted-title").on('keyup input', function (event) {
+    var val = jQuery(this).val();
+    var len = val.length;
+    var count = jQuery(this).siblings('span');
+    if (len == 0) { count.text("0/50"); return; }
+    if (len > 50) {
+        len = 50;
+        jQuery(this).val(val.substring(0, 50));
+    }
+    count.text(len + "/50");
+});
