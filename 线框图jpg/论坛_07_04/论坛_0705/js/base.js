164d10097c5434cd492805df1dc2ef81772f9116
jeawyzhang <281475120@163.com>
2017-12-09 18:21:29 +0800

    优化

--- 线框图jpg/论坛_07_04/论坛_0705/js/base.js
+++ 线框图jpg/论坛_07_04/论坛_0705/js/base.js
@@ -21,3 +21,25 @@ jQuery("#posted-title").on('keyup input', function (event) {
     }
     count.text(len + "/50");
 });
+
+/* 底部菜单点击显示隐藏二级菜单 */
+jQuery('#forum').click(function(){
+    jQuery('.hide-menu').toggle();
+})
+
+/* 回到顶部 */
+window.onload = function(){
+    var oTop = document.getElementById("to_top");
+    var screenh = jQuery(window).height()/2;
+    window.onscroll = function(){
+      var scrolltop =document.documentElement.scrollTop;
+      if(screenh<scrolltop){
+        jQuery(oTop).show();
+      }else{
+        jQuery(oTop).hide();
+      };
+    };
+    oTop.onclick = function(){
+      document.documentElement.scrollTop = document.body.scrollTop =0;
+    };
+  };
