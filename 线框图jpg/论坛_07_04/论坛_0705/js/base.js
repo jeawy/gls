da18e575c60cf051efba743fe3c21e9fe0bbae73
jeawyzhang <281475120@163.com>
2017-12-19 20:52:42 +0800

    管理页静态

--- 线框图jpg/论坛_07_04/论坛_0705/js/base.js
+++ 线框图jpg/论坛_07_04/论坛_0705/js/base.js
@@ -1,10 +1,5 @@
 
-/* 点击按钮链接微博 */
-jQuery('#microblog').click(function() {
-location.href = 'https://weibo.com/asu001';
-})
-
-/* 下拉框 */
+/* 下拉框宽度 */
 jQuery(document).ready(function(){
 jQuery('ul.spinner').width(jQuery('button.spinner_btn').innerWidth());
 });
